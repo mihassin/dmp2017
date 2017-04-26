dddb487b1c6208ff6df5c82e7ac77ea83e4c5a35
mihassin <marko.hassinen@helsinki.fi>
2017-04-26 14:41:36 +0300

    data preprocessing

--- src/codebook.py
+++ src/codebook.py
@@ -3,119 +3,193 @@ from helper import flatten
 
 
 class CodeBook:
+	def __init__(self):
+
 	# REMEMBER TO REMOVE MISSING VALUES
 
 	# Index 0
 	# Light: Mopeds/Motocycles/Quads etc.
 	# Heavy: Tractor/Buss/Van/Lorry
 	# Others: Others/Public works vehicles/All-terrain vehicles
-	__vclasses__ = {'Light': ['L1', 'L1e', 'L2', 'L2e', 'L3', 'L3e', 'L4', 'L4e', 'L5', 'L5e', 'L6e', 'L7e', 'KNP'],
+	 self._vclasses = {'Light': ['L1', 'L1e', 'L2', 'L2e', 'L3', 'L3e', 'L4', 'L4e', 'L5', 'L5e', 'L6e', 'L7e', 'KNP'],
 					'Car': ['M1', 'M1G'],
 					'Trailer': ['O1', 'O2', 'O3', 'O4'],
 					'Heavy': ['C1', 'C2', 'T', 'T1', 'T2', 'T3', 'T4', 'T5', 'LTR', 'M2', 'M2G', 'M3', 'N1', 'N1G', 'N2', 'N2G', 'N3', 'N3G'],
 					'Others': ['MUU', 'MTK', 'MA']}
 
 	# Index 1
-	__init_regis__ = ['1990 - 1999', '1958 - 1979', '1980 - 1989', '2010 - 2016', '2000 - 2009']
+	 self._init_regis = ['ir 1990 - 1999', 'ir 1958 - 1979', 'ir 1980 - 1989', 'ir 2010 - 2016', 'ir 2000 - 2009']
 
 	# Index 3
 	# Remove missing and nul values
-	__usages__ = {'Private': ['01'], 'Subject to permit': ['02'], 'School vechile': ['03'], 'Rental': ['04'], 'Sales storage': ['05']}
+	 self._usages = {'Private': ['01'], 'Subject to permit': ['02'], 'School vechile': ['03'], 'Rental': ['04'], 'Sales storage': ['05']}
 
 	# Index 6
 	# remove 00000000, remove 00050812
-	__commencement__ = ['1900 - 1989', '1990 - 1999', '2000 - 2004', '2005 - 2009', '2010 - 2017']
+	 self._commencement = ['cy 1900 - 1989', 'cy 1990 - 1999', 'cy 2000 - 2004', 'cy 2005 - 2009', 'cy 2010 - 2017']
 
 	# Index 7
-	__colors__ = {'Black': ['0'], 'Brown': ['1'], 'Red': ['2'], 'Green': ['5'],
+	 self._colors = {'Black': ['0'], 'Brown': ['1'], 'Red': ['2'], 'Green': ['5'],
  			  	  'Blue': ['6', 'Z'], 'Grey': ['8'], 'White': ['9'], 'Silver': ['Y'],
- 			  	  'Others': ['3', '4', '7', 'X']}
+ 			  	  'Other colors': ['3', '4', '7', 'X']}
 
  	# Index 8
-	__doors__ = ['Less than 4', '4', '5' 'More than 5']
+	 self._doors = {'Less than 4 doors': {'max': 4}, '4 doors': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 doors': {'eq': 5, 'min': 1e30, 'max': -1e30}, 'More than 5 doors': {'min': 6}}
 
 	# Index 11
 	# Remove value 0 - occurs only once in data
-	__seats__ = ['1', '2', '3', '4', '5', 'More than 5']
+	 self._seats = {'1 seat': {'eq': 1,  'min': 1e30, 'max': -1e30}, '2 seats': {'eq': 2,  'min': 1e30, 'max': -1e30}, '3 seats': {'eq': 3,  'min': 1e30, 'max': -1e30},
+				 '4 seats': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 seats': {'eq': 5,  'min': 1e30, 'max': -1e30}, 'More than 5 seats': {'min': 6}}
 
 	# Index 12
 	# min < val <= max
-	__mass__ = {'0 - 1000 kg': {'max': 1000}, '1000 - 1500 kg': {'min': 1000, 'max': 1500}, '1500 - 2000 kg': {'min': 1500, 'max': 2000}, 'Greater': {'min': 2000}}
+	 self._mass = {'0 - 1000 kg': {'max': 1000}, '1000 - 1500 kg': {'min': 1000, 'max': 1500}, '1500 - 2000 kg': {'min': 1500, 'max': 2000}, 'Greater than 2000 kg': {'min': 2000}}
 
 	# Index 15
 	# min < val <= max
-	__length__ = {'0 - 4300 mm': {'max': 4300}, '4300 - 4700 mm': {'min': 4300, 'max': 4700}, 'Longer than 4700 mm': {'min': 4700}}
+	 self._length = {'0 - 4300 mm': {'max': 4300}, '4300 - 4700 mm': {'min': 4300, 'max': 4700}, 'Longer than 4700 mm': {'min': 4700}}
 
 	# Index 16
 	# min < val <= max
-	__width__ = {'0 - 1700 mm': {'max': 1700}, '1700 - 1800 mm': {'min': 1700, 'max': 1800}, '1800 - 1900 mm': {'min': 1800, 'max': 1900}, '1900 - 2000 mm': {'min': 1900, 'max': 2000}, 'Wider than 2000 mm': {'min': 2000}}
+	 self._width = {'0 - 1700 mm': {'max': 1700}, '1700 - 1800 mm': {'min': 1700, 'max': 1800}, '1800 - 1900 mm': {'min': 1800, 'max': 1900}, '1900 - 2000 mm': {'min': 1900, 'max': 2000}, 'Wider than 2000 mm': {'min': 2000}}
 
 	# Index 17
 	# min < val <= max
-	__height__ = {'0 - 1450 mm': {'max': 1450}, '1450 - 1500 mm': {'min': 1450, 'max': 1500}, '1500 - 1550 mm': {'min': 1500, 'max': 1550}, 'Greater than 1550 mm': {'min': 1550}}
+	 self._height = {'0 - 1450 mm': {'max': 1450}, '1450 - 1500 mm': {'min': 1450, 'max': 1500}, '1500 - 1550 mm': {'min': 1500, 'max': 1550}, 'Greater than 1550 mm': {'min': 1550}}
 
 	# Index 18
-	__fuels__ = {'Gasoline': ['01'], 'Diesel': ['02'],
-			     'Others': ['33', '05', '53', '11', '13', '63', '48',
+	 self._fuels = {'Gasoline': ['01'], 'Diesel': ['02'],
+			     'Other fuels': ['33', '05', '53', '11', '13', '63', '48',
 			 				'39', '61', '04', 'Y', '60', '37', '43',
 			 				'59', '06', '40', '42', '58', '34', '44',
 			 				'32', '38', '03', '31', '67', '47']}
 
 	# Index 19
 	# min < val <= max
-	__displacement__ = {'Small': {'max': 1000}, 'Large': {'min': 2999}, '1000 - 1999 cc': {'min': 999, 'max': 2000}, '2000 - 2999 cc': {'min': 1999, 'max': 3000}}
+	 self._displacement = {'Small engine': {'max': 1000}, 'Large engine': {'min': 2999}, '1000 - 1999 cc': {'min': 999, 'max': 2000}, '2000 - 2999 cc': {'min': 1999, 'max': 3000}}
 
 	# Index 20
 	# min < val <= max
-	__power__ = {'0 - 50 kW': {'max': 50}, '50 - 75 kW': {'min': 50, 'max': 75}, '75 - 100 kW': {'min': 75, 'max': 100},
-			 '100 - 150 kW': {'min': 100, 'max': 150}, '150 - 200 kW': {'min': 150, 'max': 200}, 'Greater': {'min': 200}}
+	 self._power = {'0 - 50 kW': {'max': 50}, '50 - 75 kW': {'min': 50, 'max': 75}, '75 - 100 kW': {'min': 75, 'max': 100},
+			 '100 - 150 kW': {'min': 100, 'max': 150}, '150 - 200 kW': {'min': 150, 'max': 200}, 'Greater than 200 kW': {'min': 200}}
 
 	# Index 21
-	__cylinders__ = ['Less than 4', '4', 'More than 4']
-	
+	 self._cylinders = {'Less than 4 cylinders': {'max': 4}, '4 cylinders':{'eq': 4, 'min': 1e30, 'max': -1e30}, 'More than 4 cylinders': {'min': 5}}
+
 	# Index 22
-	__supercharger__ = {'Supercharger': 'true', 'No supercharger': 'false'}
+	 self._supercharger = {'Supercharger': 'true', 'No supercharger': 'false'}
 
 	# Index 23
-	__hybrid__ = {'Hybrid': 'true', 'Not hybrid' :'false'}
+	 self._hybrid = {'Hybrid': 'true', 'Not hybrid' :'false'}
 
 	# Index 33
 	# min < val <= max
-	__co2__ = {'0 - 150 g': {'max': 150}, '150 - 200 g': {'min': 150, 'max': 200}, 'More than 200 g': {'min': 200}}
-	
+	 self._co2 = {'0 - 150 g': {'max': 150}, '150 - 200 g': {'min': 150, 'max': 200}, 'More than 200 g': {'min': 200}}
+
 	# Index 34
 	# min < val <= max
-	__km__ = {'More than 200000 km':  {'min': 100000, 'max': 200000}, '0 - 100000 km': {'max': 100000}, '100000 - 200000 km':  {'min': 200000}}
+	 self._km = {'More than 200000 km':  {'min': 100000, 'max': 200000}, '0 - 100000 km': {'max': 100000}, '100000 - 200000 km':  {'min': 200000}}
 
 
-	__classes__ = flatten(
-				  			[__vclasses__.keys(), __init_regis__, __usages__.keys(), __commencement__,
-				   			 __colors__.keys(), __doors__, __seats__, __mass__.keys(), __length__.keys(),
-				   			 __length__.keys(), __width__.keys(), __height__.keys(), __fuels__.keys(),
-				   			 __displacement__.keys(), __power__.keys(), __cylinders__, __supercharger__.keys(),
-				   			 __hybrid__.keys(), __co2__.keys(), __km__.keys()]
+	 self._classes = flatten(
+				  		[self._vclasses.keys(), self._init_regis, self._usages.keys(), self._commencement,
+				   		 self._colors.keys(), self._doors.keys(), self._seats.keys(), self._mass.keys(),
+				   		 self._length.keys(), self._width.keys(), self._height.keys(), self._fuels.keys(),
+				   		 self._displacement.keys(), self._power.keys(), self._cylinders.keys(),
+				   		 self._supercharger.keys(), self._hybrid.keys(), self._co2.keys(), self._km.keys()]
 				   		)
 
+	def __handle_dict(self, value, codes):
+		for k, v in codes.items():
+			if value in v:
+				return k
 
-	def classify(data):
-		return 
+	def __handle_dict_interval(self, value, codes):
+		val = float(value)
+		for k, v in codes.items():
+			try:
+				eq = v['eq']
+			except KeyError:
+				eq = -1e30
+			if val == eq:
+				return k
+			try:
+				minv = v['min']
+			except KeyError:
+				minv = 0
+			try:
+				maxv = v['max']
+			except KeyError:
+				maxv = 1e30
+			if val >= minv and val < maxv:
+				return k 
+
+	def __handle_date_interval(self, value, codes):
+		year = value[:4]
+		for interval in codes:
+			tmp = interval[3:].split(' - ')
+			minv = tmp[0]
+			maxv = tmp[1]
+			if year >= minv and year <= maxv:
+				return interval
 
 
-	def integerify(data):
+	def classify(self, data):
+		'''Changes raw data values into
+		selected item names.
+
+		data -- preprocessed data matrix
+		'''
+		cl_data = []
+		for row in data:
+			classified = []
+			classified.append(self.__handle_dict(row[0], self._vclasses))
+			classified.append(self.__handle_date_interval(row[1], self._init_regis))
+			classified.append(self.__handle_dict(row[2], self._usages))
+			classified.append(self.__handle_date_interval(row[3], self._commencement))
+			classified.append(self.__handle_dict(row[4], self._colors))
+			classified.append(self.__handle_dict_interval(row[5], self._doors))
+			classified.append(self.__handle_dict_interval(row[6], self._seats))
+			classified.append(self.__handle_dict_interval(row[7], self._mass))
+			classified.append(self.__handle_dict_interval(row[8], self._length))
+			classified.append(self.__handle_dict_interval(row[9], self._width))
+			classified.append(self.__handle_dict_interval(row[10], self._height))
+			classified.append(self.__handle_dict(row[11], self._fuels))
+			classified.append(self.__handle_dict_interval(row[12], self._displacement))
+			classified.append(self.__handle_dict_interval(row[13], self._power))
+			classified.append(self.__handle_dict_interval(row[14], self._cylinders))
+			classified.append(self.__handle_dict(row[15], self._supercharger))
+			classified.append(self.__handle_dict(row[16], self._hybrid))
+			classified.append(self.__handle_dict_interval(row[17], self._co2))
+			classified.append(self.__handle_dict_interval(row[18], self._km))
+			cl_data.append(classified)
+		return cl_data
+
+
+	def integerify(self, data):
+		'''Further simplifies item names into integers.
+
+		data -- classified data matrix
+		'''
 		int_data = []
 		for i in range(len(data)):
 			row = data[i]
 			int_data.append([])
 			for j in range(len(row)):
-				int_data[i].append(cb.__classes__.index(row[j]))
+				int_data[i].append(self._classes.index(row[j]))
 		return int_data
 
 
-	def stringify(data):
+	def stringify(self, data):
+		'''To be used after data mining procedures.
+		Returns integer values into more readable item names.
+		
+		data -- data matrix with integer values
+		'''
 		str_data = []
 		for i in range(len(data)):
 			row = data[i]
 			str_data.append([])
 			for j in range(len(row)):
-				str_data[i].append(cb.__classes__[row[j]])
+				str_data[i].append(self._classes[row[j]])
 		return str_data
--- src/eclat.py
+++ src/eclat.py
@@ -1,12 +1,31 @@
-from helper import transpose
+from interestingness_measures import support_count
+
+
+def one_item_sets(D):
+    I = set()
+    for transaction in D:
+        I = I.union(transaction)
+    return I
 
 
 def eclat(F, X, I, D, minsupcount):
+    """Implementation of Eclat algorithm.
+    Searches for frequent itemsets in D with
+    minimum support count minsupcount and 
+    stores found sets in F.
+
+    Arguments:
+    F -- storage for frequent itemsets
+    X -- (k-1) previous items (prefix pattern)
+    I -- all 1-itemsets of D
+    D -- data set
+    minsupcount -- minimum support count
+    """
     if I == set():
         return set()
     frequent = []
     for item in I:
-        candidate = X.union(item)
+        candidate = X.union([item])
         sc = support_count(candidate, D)
         if(sc >= minsupcount):
             frequent.append((candidate, item, sc))
@@ -17,15 +36,15 @@ def eclat(F, X, I, D, minsupcount):
 
 
 def main(): 
-    D = ['abc', 'acdef', 'abc', 'de']
+    # D = ['abc', 'acdef', 'abc', 'de']
+    # D = [['aa', 'ab', 'ac'], ['aa', 'ac', 'ad', 'ae', 'af'], ['aa', 'ab', 'ac'], ['ad', 'ae']]
+    D = [[1, 2, 3], [1, 3, 4, 5, 6], [1, 2, 3], [4, 5]]
     print("Transaction data:")
     print(D, "\n")
-    I = set()
-    for transaction in D:
-        I = I.union(transaction)
+    I = one_item_sets(D)
     frequent_itemsets = []
     eclat(frequent_itemsets, set(), I, D, 2)
-    print("Frequent itemsets with support count threshold of 2:")
+    print("Found frequent itemsets with frequency of 2:")
     print(frequent_itemsets)
 
 if __name__ == "__main__":
