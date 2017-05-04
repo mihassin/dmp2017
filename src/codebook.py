9f0016462db97fb4f9231054dd0272629a84de6a
mihassin <marko.hassinen@helsinki.fi>
2017-05-04 23:42:15 +0300

    preprocessor into class

--- src/codebook.py
+++ src/codebook.py
@@ -3,114 +3,117 @@ from helper import flatten
 
 
 class CodeBook:
-	'''CodeBook includes all necessary information regarding the different
-	categories used for the data attributes. Class has alot of hardcoding
-	based on the analysis of the data done before the data mining. There are
-	also few simple functions used for data transformation.
+	'''CodeBook includes all necessary information and
+	functions for turning the raw data into mineable form.
 	'''
 
-
 	def __init__(self):
-	# REMEMBER TO REMOVE MISSING VALUES
+		# Index 0
+		# Light: Mopeds/Motocycles/Quads etc.
+		# Heavy: Tractor/Buss/Van/Lorry
+		# Others: Others/Public works vehicles/All-terrain vehicles
+		self._vclasses = {'Light': ['L1', 'L1e', 'L2', 'L2e', 'L3', 'L3e', 'L4', 'L4e', 'L5', 'L5e', 'L6e', 'L7e', 'KNP'],
+						'Car': ['M1', 'M1G'],
+						'Trailer': ['O1', 'O2', 'O3', 'O4'],
+						'Heavy': ['C1', 'C2', 'T', 'T1', 'T2', 'T3', 'T4', 'T5', 'LTR', 'M2', 'M2G', 'M3', 'N1', 'N1G', 'N2', 'N2G', 'N3', 'N3G'],
+						'Others': ['MUU', 'MTK', 'MA']}
 
-	# Index 0
-	# Light: Mopeds/Motocycles/Quads etc.
-	# Heavy: Tractor/Buss/Van/Lorry
-	# Others: Others/Public works vehicles/All-terrain vehicles
-	 self._vclasses = {'Light': ['L1', 'L1e', 'L2', 'L2e', 'L3', 'L3e', 'L4', 'L4e', 'L5', 'L5e', 'L6e', 'L7e', 'KNP'],
-					'Car': ['M1', 'M1G'],
-					'Trailer': ['O1', 'O2', 'O3', 'O4'],
-					'Heavy': ['C1', 'C2', 'T', 'T1', 'T2', 'T3', 'T4', 'T5', 'LTR', 'M2', 'M2G', 'M3', 'N1', 'N1G', 'N2', 'N2G', 'N3', 'N3G'],
-					'Others': ['MUU', 'MTK', 'MA']}
+		# Index 1
+		self._init_regis = ['ir 1990 - 1999', 'ir 1958 - 1979', 'ir 1980 - 1989', 'ir 2010 - 2016', 'ir 2000 - 2009']
 
-	# Index 1
-	 self._init_regis = ['ir 1990 - 1999', 'ir 1958 - 1979', 'ir 1980 - 1989', 'ir 2010 - 2016', 'ir 2000 - 2009']
+		# Index 3
+		self._usages = {'Private': ['01'], 'Subject to permit': ['02'], 'School vechile': ['03'], 'Rental': ['04'], 'Sales storage': ['05']}
 
-	# Index 3
-	# Remove missing and nul values
-	 self._usages = {'Private': ['01'], 'Subject to permit': ['02'], 'School vechile': ['03'], 'Rental': ['04'], 'Sales storage': ['05']}
+		# Index 6
+		self._commencement = ['cy 1900 - 1989', 'cy 1990 - 1999', 'cy 2000 - 2004', 'cy 2005 - 2009', 'cy 2010 - 2017']
 
-	# Index 6
-	# remove 00000000, remove 00050812
-	 self._commencement = ['cy 1900 - 1989', 'cy 1990 - 1999', 'cy 2000 - 2004', 'cy 2005 - 2009', 'cy 2010 - 2017']
+		# Index 7
+		self._colors = {'Black': ['0'], 'Brown': ['1'], 'Red': ['2'], 'Green': ['5'],
+					'Blue': ['6', 'Z'], 'Grey': ['8'], 'White': ['9'], 'Silver': ['Y'],
+					'Other colors': ['3', '4', '7', 'X']}
 
-	# Index 7
-	 self._colors = {'Black': ['0'], 'Brown': ['1'], 'Red': ['2'], 'Green': ['5'],
- 			  	  'Blue': ['6', 'Z'], 'Grey': ['8'], 'White': ['9'], 'Silver': ['Y'],
- 			  	  'Other colors': ['3', '4', '7', 'X']}
+ 		# Index 8
+		self._doors = {'Less than 4 doors': {'max': 4}, '4 doors': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 doors': {'eq': 5, 'min': 1e30, 'max': -1e30}, 'More than 5 doors': {'min': 6}}
 
- 	# Index 8
-	 self._doors = {'Less than 4 doors': {'max': 4}, '4 doors': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 doors': {'eq': 5, 'min': 1e30, 'max': -1e30}, 'More than 5 doors': {'min': 6}}
+		# Index 11
+		self._seats = {'1 seat': {'eq': 1,  'min': 1e30, 'max': -1e30}, '2 seats': {'eq': 2,  'min': 1e30, 'max': -1e30}, '3 seats': {'eq': 3,  'min': 1e30, 'max': -1e30},
+					'4 seats': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 seats': {'eq': 5,  'min': 1e30, 'max': -1e30}, 'More than 5 seats': {'min': 6}}
 
-	# Index 11
-	# Remove value 0 - occurs only once in data
-	 self._seats = {'1 seat': {'eq': 1,  'min': 1e30, 'max': -1e30}, '2 seats': {'eq': 2,  'min': 1e30, 'max': -1e30}, '3 seats': {'eq': 3,  'min': 1e30, 'max': -1e30},
-				 '4 seats': {'eq': 4,  'min': 1e30, 'max': -1e30}, '5 seats': {'eq': 5,  'min': 1e30, 'max': -1e30}, 'More than 5 seats': {'min': 6}}
+		# Index 12
+		# min < val <= max
+		self._mass = {'0 - 1000 kg': {'max': 1000}, '1000 - 1500 kg': {'min': 1000, 'max': 1500}, '1500 - 2000 kg': {'min': 1500, 'max': 2000}, 'Greater than 2000 kg': {'min': 2000}}
 
-	# Index 12
-	# min < val <= max
-	 self._mass = {'0 - 1000 kg': {'max': 1000}, '1000 - 1500 kg': {'min': 1000, 'max': 1500}, '1500 - 2000 kg': {'min': 1500, 'max': 2000}, 'Greater than 2000 kg': {'min': 2000}}
+		# Index 15
+		# min < val <= max
+		self._length = {'0 - 4300 mm': {'max': 4300}, '4300 - 4700 mm': {'min': 4300, 'max': 4700}, 'Longer than 4700 mm': {'min': 4700}}
 
-	# Index 15
-	# min < val <= max
-	 self._length = {'0 - 4300 mm': {'max': 4300}, '4300 - 4700 mm': {'min': 4300, 'max': 4700}, 'Longer than 4700 mm': {'min': 4700}}
+		# Index 16
+		# min < val <= max
+		self._width = {'0 - 1700 mm': {'max': 1700}, '1700 - 1800 mm': {'min': 1700, 'max': 1800}, '1800 - 1900 mm': {'min': 1800, 'max': 1900},
+					'1900 - 2000 mm': {'min': 1900, 'max': 2000}, 'Wider than 2000 mm': {'min': 2000}}
 
-	# Index 16
-	# min < val <= max
-	 self._width = {'0 - 1700 mm': {'max': 1700}, '1700 - 1800 mm': {'min': 1700, 'max': 1800}, '1800 - 1900 mm': {'min': 1800, 'max': 1900}, '1900 - 2000 mm': {'min': 1900, 'max': 2000}, 'Wider than 2000 mm': {'min': 2000}}
+		# Index 17
+		# min < val <= max
+		self._height = {'0 - 1450 mm': {'max': 1450}, '1450 - 1500 mm': {'min': 1450, 'max': 1500}, '1500 - 1550 mm': {'min': 1500, 'max': 1550}, 'Greater than 1550 mm': {'min': 1550}}
 
-	# Index 17
-	# min < val <= max
-	 self._height = {'0 - 1450 mm': {'max': 1450}, '1450 - 1500 mm': {'min': 1450, 'max': 1500}, '1500 - 1550 mm': {'min': 1500, 'max': 1550}, 'Greater than 1550 mm': {'min': 1550}}
+		# Index 18
+		self._fuels = {'Gasoline': ['01'], 'Diesel': ['02'],
+					'Other fuels': ['33', '05', '53', '11', '13', '63', '48',
+									'39', '61', '04', 'Y', '60', '37', '43',
+									'59', '06', '40', '42', '58', '34', '44',
+									'32', '38', '03', '31', '67', '47']}
 
-	# Index 18
-	 self._fuels = {'Gasoline': ['01'], 'Diesel': ['02'],
-			     'Other fuels': ['33', '05', '53', '11', '13', '63', '48',
-			 				'39', '61', '04', 'Y', '60', '37', '43',
-			 				'59', '06', '40', '42', '58', '34', '44',
-			 				'32', '38', '03', '31', '67', '47']}
+		# Index 19
+		# min < val <= max
+		self._displacement = {'Small engine': {'max': 1000}, 'Large engine': {'min': 2999}, '1000 - 1999 cc': {'min': 999, 'max': 2000}, '2000 - 2999 cc': {'min': 1999, 'max': 3000}}
 
-	# Index 19
-	# min < val <= max
-	 self._displacement = {'Small engine': {'max': 1000}, 'Large engine': {'min': 2999}, '1000 - 1999 cc': {'min': 999, 'max': 2000}, '2000 - 2999 cc': {'min': 1999, 'max': 3000}}
+		# Index 20
+		# min < val <= max
+		self._power = {'0 - 50 kW': {'max': 50}, '50 - 75 kW': {'min': 50, 'max': 75}, '75 - 100 kW': {'min': 75, 'max': 100},
+					 '100 - 150 kW': {'min': 100, 'max': 150}, '150 - 200 kW': {'min': 150, 'max': 200}, 'Greater than 200 kW': {'min': 200}}
 
-	# Index 20
-	# min < val <= max
-	 self._power = {'0 - 50 kW': {'max': 50}, '50 - 75 kW': {'min': 50, 'max': 75}, '75 - 100 kW': {'min': 75, 'max': 100},
-			 '100 - 150 kW': {'min': 100, 'max': 150}, '150 - 200 kW': {'min': 150, 'max': 200}, 'Greater than 200 kW': {'min': 200}}
+		# Index 21
+		self._cylinders = {'Less than 4 cylinders': {'max': 4}, '4 cylinders':{'eq': 4, 'min': 1e30, 'max': -1e30}, 'More than 4 cylinders': {'min': 5}}
 
-	# Index 21
-	 self._cylinders = {'Less than 4 cylinders': {'max': 4}, '4 cylinders':{'eq': 4, 'min': 1e30, 'max': -1e30}, 'More than 4 cylinders': {'min': 5}}
+		# Index 22
+		self._supercharger = {'Supercharger': 'true', 'No supercharger': 'false'}
 
-	# Index 22
-	 self._supercharger = {'Supercharger': 'true', 'No supercharger': 'false'}
+		# Index 23
+		self._hybrid = {'Hybrid': 'true', 'Not hybrid' :'false'}
 
-	# Index 23
-	 self._hybrid = {'Hybrid': 'true', 'Not hybrid' :'false'}
+		# Index 33
+		# min < val <= max
+		self._co2 = {'0 - 150 g': {'max': 150}, '150 - 200 g': {'min': 150, 'max': 200}, 'More than 200 g': {'min': 200}}
 
-	# Index 33
-	# min < val <= max
-	 self._co2 = {'0 - 150 g': {'max': 150}, '150 - 200 g': {'min': 150, 'max': 200}, 'More than 200 g': {'min': 200}}
-
-	# Index 34
-	# min < val <= max
-	 self._km = {'More than 200000 km':  {'min': 100000, 'max': 200000}, '0 - 100000 km': {'max': 100000}, '100000 - 200000 km':  {'min': 200000}}
+		# Index 34
+		# min < val <= max
+		self._km = {'More than 200000 km':  {'min': 100000, 'max': 200000}, '0 - 100000 km': {'max': 100000}, '100000 - 200000 km':  {'min': 200000}}
 
 
-	 self._classes = flatten(
-				  		[self._vclasses.keys(), self._init_regis, self._usages.keys(), self._commencement,
-				   		 self._colors.keys(), self._doors.keys(), self._seats.keys(), self._mass.keys(),
-				   		 self._length.keys(), self._width.keys(), self._height.keys(), self._fuels.keys(),
-				   		 self._displacement.keys(), self._power.keys(), self._cylinders.keys(),
-				   		 self._supercharger.keys(), self._hybrid.keys(), self._co2.keys(), self._km.keys()]
-				   		)
+		self._classes = flatten(
+						[self._vclasses.keys(), self._init_regis, self._usages.keys(), self._commencement,
+						self._colors.keys(), self._doors.keys(), self._seats.keys(), self._mass.keys(),
+						self._length.keys(), self._width.keys(), self._height.keys(), self._fuels.keys(),
+						self._displacement.keys(), self._power.keys(), self._cylinders.keys(),
+						self._supercharger.keys(), self._hybrid.keys(), self._co2.keys(), self._km.keys()]
+						)
 
 	def __handle_dict(self, value, codes):
+		'''Changes raw data values into mineable classes
+
+		value -- raw value in data matrix
+		codes -- class codes for attributes
+		'''
 		for k, v in codes.items():
 			if value in v:
 				return k
 
 	def __handle_dict_interval(self, value, codes):
+		'''Changes raw data values into mineable classes
+
+		value -- raw value in data matrix
+		codes -- class codes for attributes
+		'''
 		val = float(value)
 		for k, v in codes.items():
 			try:
@@ -131,6 +134,11 @@ class CodeBook:
 				return k 
 
 	def __handle_date_interval(self, value, codes):
+		'''Changes raw data values into mineable classes
+
+		value -- raw value in data matrix
+		codes -- class codes for attributes
+		'''
 		year = value[:4]
 		for interval in codes:
 			tmp = interval[3:].split(' - ')
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -6,134 +6,152 @@ import os
 from helper import transpose
 from codebook import CodeBook
 
-# Globally accessible file_name and data_file
-__file_name__ = "Tieliikenne_AvoinData_4_8.zip"
-__data_file__ = "vehicledata.csv"
 
-cb = CodeBook()
+class Preprocessor:
+	'''Preprocessor class is responsible for 
+	altering data into data mineable form.
+	'''
 
-
-def fetch_data():
-    """Function that fetches the project data from hard-coded location.
-    The zip file will be stored in ../target/
-    """
-    url = "http://trafiopendata.97.fi/opendata/" + __file_name__
-    req = urllib.request.urlopen(url)
-    meta = req.info()
-    storing_location = "../target/" + __file_name__
-    file_size = int(meta.get("Content-Length"))
-    print("Downloading: %s Bytes: %s" % (__file_name__, file_size))
-    with open(storing_location, 'wb') as f:
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = req.read(block_sz)
-            if not buffer: break
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            print(status)
+	def __init__(self, path):
+		if not os.path.exists(path):
+			os.makedirs(path)
+		self._path = path
+		self._file_name = "Tieliikenne_AvoinData_4_8.zip"
+		self._data_file = "vehicledata.csv"
+		self._cb = CodeBook()
+		self._file_location = ''
+		self._data_location = self._path+self._data_file
 
 
-def extract_data():
-    """Extracts the data file.
-    """
-    path = '../target/'
-    file_location = path + __file_name__
-    if not zipfile.is_zipfile(file_location):
-        print("File in location " + file_location + " is not a zip file!\n"
-              "Check the zip file or try re-fetching the file.")
-        return
-    zp = zipfile.ZipFile(file_location)
-    data_file = zp.namelist()[0]
-    zp.extract(data_file, path)
-    zp.close()
-    os.rename(path+data_file, path+__data_file__)
+	def fetch_data(self):
+		'''Function that fetches the project data from hard-coded location.
+		The zip file will be stored in ../target/
+		'''
+		if os.path.isfile(self._data_location):
+			print('It seems that the data dile already exists!', self._data_location)
+			return
+		url = "http://trafiopendata.97.fi/opendata/" + self._file_name
+		req = urllib.request.urlopen(url)
+		meta = req.info()
+		self._file_location = self._path + self._file_name
+		file_size = int(meta.get("Content-Length"))
+		print("Downloading: %s Bytes: %s" % (self._file_name, file_size))
+		with open(self._file_location, 'wb') as f:
+			file_size_dl = 0
+			block_sz = 8192
+			while True:
+				buffer = req.read(block_sz)
+				if not buffer: break
+				file_size_dl += len(buffer)
+				f.write(buffer)
+				status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+				status = status + chr(8)*(len(status)+1)
+				print(status)
+			print()
+			self._extract_data()
 
 
-def cleanse_and_transform():
-    '''Removes columns, that are not suitable or interesting for data mining.
-    Removes all rows containing null values. Transforms all values into categories,
-    splits each category into their own binary attribute. Each of the binary attributes
-    have integer label.
-    '''
-    data = []
-    with open('../target/' + __data_file__, encoding='latin_1') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        next(reader)
-        for row in reader:
-            row = trim_features(row)
-            if row.__contains__(''): continue
-            data.append(row)
-        data = cb.classify(data)
-        data = cb.integerify(data)
-    return data
+	def _extract_data(self):
+		'''Extracts the data file.'''
+		print('Initiating extraction')
+		if not zipfile.is_zipfile(self._file_location):
+			print("File in location " + self._file_location + " is not a zip file!\n"
+			"Check the zip file or try re-fetching the file.")
+			return
+		zp = zipfile.ZipFile(self._file_location)
+		data_file = zp.namelist()[0]
+		zp.extract(data_file, self._path)
+		zp.close()
+		os.rename(self._path+data_file, self._data_location)
+		print('Extraction complete!')
 
 
-def trim_features(row):
-    '''Removes unsuitable columns from the row
-    
-    Argument:
-    row -- a row in the data matrix
-    ''' 
-    trimmed = row[0:2]
-    trimmed.append(row[3])
-    trimmed.extend(row[6:9])
-    trimmed.extend(row[11:13])
-    trimmed.extend(row[15:24])
-    trimmed.extend(row[33:35])
-    return trimmed
-
-def read_column(attribute = 0):
-    """Reads a column from the data file.
-    Skips the headings.
-
-    attribute -- index value of the desired column
-    """
-    data = [] 
-    with open('../target/' + __data_file__, encoding="latin_1") as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        next(reader)
-        for row in reader:
-            data.append(row[attribute])
-    return data
+	def cleanse_and_transform(self):
+		'''Removes columns, that are not suitable or interesting for data mining.
+		Removes all rows containing null values. Transforms all values into categories,
+		splits each category into their own binary attribute. Each of the binary attributes
+		have integer label.
+		'''
+		data = []
+		with open(self._data_location, encoding='latin_1') as csvfile:
+			reader = csv.reader(csvfile, delimiter=';')
+			next(reader)
+			for row in reader:
+				row = self._trim_features(row)
+				if row.__contains__(''): continue
+				data.append(row)
+			data = self._cb.classify(data)
+			data = self._cb.integerify(data)
+		return data
 
 
-def col_distribution(col):
-    '''Counts each distinct item in col
-    and returns a dictionary, where keys are the
-    col items and values their counts.
+	def _trim_features(self, row):
+		'''Removes unsuitable columns from the row
 
-    col -- a list containing data column
-    '''
-    distr = {}
-    for c in col:
-        if c not in distr:
-            distr[c] = 1
-        else:
-            distr[c] += 1
-    return distr
+		row -- a row in the data matrix
+		''' 
+		trimmed = row[0:2]
+		trimmed.append(row[3])
+		trimmed.extend(row[6:9])
+		trimmed.extend(row[11:13])
+		trimmed.extend(row[15:24])
+		trimmed.extend(row[33:35])
+		return trimmed
 
 
-def data_attributes():
-    '''Reads the first row of data file.
-    '''
-    with open('../target/' + __data_file__, encoding="latin_1") as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        attributes = next(reader)
-    return attributes
+	def data_attributes(self):
+		'''Reads the first row of data file.'''
+		with open(self._data_location, encoding="latin_1") as csvfile:
+			reader = csv.reader(csvfile, delimiter=';')
+			attributes = next(reader)
+		return attributes
 
-def stringify(data, kind='frequent'):
-    '''The Integer values of data are indexes of list cb._classes.
-    It is important, that sthe ame instant of cb does the stringification
-    and integerization. Otherwise the ordering might vary and the results
-    will be false. This function ensures the use of the correct cb instance. 
-    '''
-    if kind == 'frequent':
-        return cb.stringify_itemsets(data)
-    elif kind == 'rules':
-        return cb.stringify_rules(data)
-    else:
-        return "Second parameter can be either 'frequent' or 'rules'!"
+
+	def col_distribution(self, column):
+		'''Counts each distinct item in column
+		and returns a dictionary, where keys are the
+		column items and values their counts.
+
+		column -- a list containing data column
+		'''
+		distr = {}
+		col = self._read_column(column)
+		for c in col:
+			if c not in distr:
+				distr[c] = 1
+			else:
+				distr[c] += 1
+		return distr
+
+
+	def _read_column(self, attribute = 0):
+		'''Reads a column from the data file.
+		Skips the headings.
+
+		attribute -- index value of the desired column
+		'''
+		data = []
+		with open(self._data_location, encoding="latin_1") as csvfile:
+			reader = csv.reader(csvfile, delimiter=';')
+			next(reader)
+			for row in reader:
+				data.append(row[attribute])
+		return data
+
+
+	def stringify(self, data, kind='frequent'):
+		'''The Integer values of data are indexes of list cb._classes.
+		It is important, that the same instant of cb does the stringification
+		and integerization. Otherwise the ordering might vary and the results
+		will be false. This function ensures the use of the correct cb instance.
+
+		data -- either frequent itemsets or association rules
+		kind -- string: either frequent or rules 
+		'''
+		if kind == 'frequent':
+			return self._cb.stringify_itemsets(data)
+		elif kind == 'rules':
+			return self._cb.stringify_rules(data)
+		else:
+			return "Second parameter can be either 'frequent' or 'rules'!"
 
